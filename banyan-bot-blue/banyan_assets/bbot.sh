ebb8e8023ad52f26350d3cae1b22b1c65f49914c
MrYsLab <MrYsLab@gmail.com>
2019-08-21 10:51:26 -0400

    Final files

---
+++ banyan-bot-blue/banyan_assets/bbot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+backplane &
+sleep 3
+sudo python3 /home/pi/crickit_gateway.py &
+sleep 5
+sudo python3 /home/pi/robot_control.py &
+sleep 2
+sudo python3 /home/pi/bluetooth_gateway.py &
+sleep 4
+
+
+
